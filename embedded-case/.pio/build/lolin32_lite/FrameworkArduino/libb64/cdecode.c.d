178a6bcc83340a11f7ac29a875f8b8f848ad2006
MrGrigorov <aleksandar.a.grigorov.2023@elsys-bg.org>
2025-03-20 13:50:31 +0200

    Added case code for testing

---
+++ embedded-case/.pio/build/lolin32_lite/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin32_lite/FrameworkArduino/libb64/cdecode.c.o: \
+ /home/alex/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ /home/alex/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
