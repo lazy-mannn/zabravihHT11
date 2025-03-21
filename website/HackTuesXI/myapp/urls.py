fd53ad7860426391be57decfe021e8b13e16accf
lazy-mannn <iliya-2009@abv.bg>
2025-03-21 09:30:49 +0200

    adding functions to call the timers

--- website/HackTuesXI/myapp/urls.py
+++ website/HackTuesXI/myapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.timer), 
+    path('', views.timer),
+    path('get-status/', views.get_status, name='get_status'),
+    path('start-work/', views.start_work, name='start_work'),
+    path('snooze/', views.snooze, name='snooze'),
 ]
--- website/HackTuesXI/myapp/views.py
+++ website/HackTuesXI/myapp/views.py
@@ -1,5 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from myapp.management.managers.timer_status_manager import TimerStatusManager, Status
+from myapp.management.managers.timer import TimerManager
 
-from django.shortcuts import render
+status_manager = TimerStatusManager()
+timer_manager = TimerManager()
+
 def timer(request):
     return render(request, "myapp/timer.html")
+
+def get_status(request):
+    return JsonResponse({
+        "status": status_manager.get_status().name,
+        "working_time_left": timer_manager.getWorkingSecCount(),
+        "rest_time_left": timer_manager.getRestSecCount()
+    })
+
+def start_work(request):
+    status_manager.set_status(Status.WORK_IN_PROGRESS)
+    return JsonResponse({"message": "Work started"})
+
+def snooze(request):
+    status_manager.set_status(Status.PAUSED)
+    timer_manager.snoozeCountdown()
+    status_manager.set_status(Status.WORK_IN_PROGRESS)
+    return JsonResponse({"message": "Snoozed and resumed"})
